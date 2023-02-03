397055180927926f26fc1f03a62703a835c3a154
MohammedAlshebl <94539750+MohammedAlshebl@users.noreply.github.com>
2023-02-03 21:30:53 +0300

    Initializing project

---
+++ src/DyslexiaFXMLController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author moham
+ */
+public class DyslexiaFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
